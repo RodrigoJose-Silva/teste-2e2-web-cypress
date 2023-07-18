c58a50ea96e5a321cc73e4b321733462b2ebcfde
Rodrigo Jose <digo.1903@hotmail.com>
2023-07-18 19:07:00 -0300

    custmizando o comando LOGIN para salvar sessão

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,32 +1,41 @@
 Cypress.Commands.add('login', (
     user = Cypress.env('user_name'),
     password = Cypress.env('user_password'),
-  ) => {
+    { cacheSession = true } = {},
+) => {
     const login = () => {
-      cy.visit('/users/sign_in')
-  
-      cy.get("[data-qa-selector='login_field']").type(user)
-      cy.get("[data-qa-selector='password_field']").type(password, { log: false })  // func "{ log: false }" não exibe o login na app
-      cy.get("[data-qa-selector='sign_in_button']").click()
-    }
-  
-    login()
-  })
+        cy.visit('/users/sign_in')
 
-  Cypress.Commands.add('logout', () => {
+        cy.get("[data-qa-selector='login_field']").type(user)
+        cy.get("[data-qa-selector='password_field']").type(password, { log: false })  // func "{ log: false }" não exibe o login na app
+        cy.get("[data-qa-selector='sign_in_button']").click()
+    }
+
+    const options = {
+        cacheAcrossSpecs: true,
+    }
+
+    if (cacheSession) {
+        cy.session(user, login, options)
+    } else {
+        login()
+    }
+})
+
+Cypress.Commands.add('logout', () => {
     const logout = () => {
-      cy.get('.qa-user-avatar').click()
-      cy.contains('Sign out').click()
+        cy.get('.qa-user-avatar').click()
+        cy.contains('Sign out').click()
     }
-  
-    logout()
-  })
 
-  Cypress.Commands.add('gui_createProject', project => {
+    logout()
+})
+
+Cypress.Commands.add('gui_createProject', project => {
     cy.visit('/projects/new')  //redirecionado diremtamente a page de new project
-  
+
     cy.get('#project_name').type(project.name)
     cy.get('#project_description').type(project.description)
     cy.get('.qa-initialize-with-readme-checkbox').check()
     cy.contains('Create project').click()
-  })
+})
